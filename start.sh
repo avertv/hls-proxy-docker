fe8ff204e005a3f68731149fb9058c588cd1d56c
Andrei <1641347+avertv@users.noreply.github.com>
2022-06-10 10:22:03 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 mkdir ./data
 #/usr/bin/docker-compose up -d "$@"
-/usr/bin/docker run -d -p 8080:8080 --restart unless-stopped -v ./data:/opt/hls-proxy --name TEST-HLS-PROXY -t -i hls-proxy "$@"
+dirlocal=$pwd
+/usr/bin/docker run -d -p 8080:8080 --restart unless-stopped -v $dirlocal/data:/opt/hls-proxy --name TEST-HLS-PROXY -t -i hls-proxy "$@"
