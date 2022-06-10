8418a562c46fd1edb2ec145bd834ea82e110ca90
Andrei <1641347+avertv@users.noreply.github.com>
2022-06-10 08:56:09 +0300

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 echo '------ DOWNLOADING HLS PROXY -------'
-wget https://www.hls-proxy.com/downloads/8.2.0/hls-proxy-8.2.0.alpine-arm64.zip -O /var/tmp/hlsproxy.zip
+wget https://www.hls-proxy.com/downloads/8.2.1-beta.1/hls-proxy-8.2.1-beta.1.linux-x64.zip -O /var/tmp/hls-proxyroxy.zip
 echo '------ UNZIP HLS PROXY -------'
-mkdir -p /var/tmp/hlsp/
-unzip -o /var/tmp/hlsproxy.zip -d /var/tmp/hlsp/
-cp /var/tmp/hlsp/* /opt/hlsp
+mkdir -p /var/tmp/hls-proxy/
+unzip -o /var/tmp/hls-proxyroxy.zip -d /var/tmp/hls-proxy/
+cp /var/tmp/hls-proxy/* /opt/hls-proxy
 echo '------ copy your groups.json -------'
-wget https://raw.githubusercontent.com/avertv/hls-proxy-docker/master/data/groups.sample.json -O /opt/hlsp/groups.json
+wget https://raw.githubusercontent.com/avertv/hls-proxy-docker/master/data/groups.json -O /opt/hls-proxy/groups.json
 echo '------ CLEAN UP TEMP -------'
-rm -rf /var/tmp/hlsp/
-rm -rf /var/tmp/hlsproxy.zip
+rm -rf /var/tmp/hls-proxy/
+rm -rf /var/tmp/hls-proxyroxy.zip
 rm -rf /var/lib/apt/lists/*
 echo '------ CHMOD HLS PROXY -------'
-chmod +x /opt/hlsp/hls-proxy
+chmod +x /opt/hls-proxy/hls-proxy
 echo '------ ALLOW HLS PROXY ON EVERY INTERFACE -------'
-/opt/hlsp/hls-proxy -address 127.0.0.1 -port 8085 -save -quit
-echo '------ SERVE HLS PROXY -------'
-/opt/hlsp/hls-proxy
+/opt/hls-proxy/hls-proxy -address 0.0.0.0 -port 8080 -save -quit
+echo '------ START HLS PROXY -------'
+/opt/hls-proxy/hls-proxy
