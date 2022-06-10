d2e31d9be30f769aeba487d55160585776279dfe
Andrei <1641347+avertv@users.noreply.github.com>
2022-06-10 09:06:17 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #! /bin/sh
-/usr/bin/docker-compose up -d "$@"
+mkdir ./data
+#/usr/bin/docker-compose up -d "$@"
+/usr/bin/docker run -d -p 8080:8080 --restart unless-stopped -v ./data:/opt/hls-proxy --name ARM7-HLS-PROXY -t -i avertv-hls-proxy
